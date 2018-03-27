597ae88f4255c39fc29addc7ea877f502562a011
Adam Zheleznyak <adam_z_2000@yahoo.com>
2018-03-26 18:32:53 -0700

    Testing

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
-  client.end();
 });
 
 const PORT = process.env.PORT || 5000;
