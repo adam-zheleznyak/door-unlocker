8744a92dc4fe074350981f6f03b0aefa9402806f
Adam Zheleznyak <adam_z_2000@yahoo.com>
2018-03-26 18:02:40 -0700

    Added a simple database

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 'use strict'
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 const PORT = process.env.PORT || 5000;
 
 const express = require('express');
