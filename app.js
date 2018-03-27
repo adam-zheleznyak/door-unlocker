997bb532ad6ab9c3c6b4e996555912c1bf2ef17f
Adam Zheleznyak <adam_z_2000@yahoo.com>
2018-03-26 18:43:12 -0700

    Testing

--- app.js
+++ app.js
@@ -7,20 +7,31 @@ const client = new Client({
   ssl: true,
 });
 
-client.connect();
-
 const PORT = process.env.PORT || 5000;
 
 const express = require('express');
 const app = express();
 
 app.get('/', function (request, response) {
+  client.connect((err) => {
+    if (err) {
+      console.error('connection error', err.stack)
+    } else {
+      console.log('connected')
+    }
+  });
   client.query('SELECT * FROM account;', (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
       response.send(JSON.stringify(row));
     }
   });
+  client.end((err) => {
+    console.log('client has disconnected')
+      if (err) {
+        console.log('error during disconnection', err.stack)
+      }
+    });
 });
 
 app.listen(PORT, function(){
